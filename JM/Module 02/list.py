348ce7cf387f01c510add56a9ef56c67c1ae6550
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2024-08-03 23:41:36 +0530

    Nothing Changed

--- JM/Module 02/list.py
+++ JM/Module 02/list.py
@@ -4,16 +4,18 @@
 list = [1,2,3,4,5,6,7,8,9]
 #index -7 -6 -5 -4 -3 -2 -1
 
-#list(start : end)
-# Prints from the start index and stops before the end index
-print(list[2:6])
-print(list[1:7])
 
-# list(start:end:step)
+
+# list(start : end)
+# Prints from the start index and stops before the end index
+# print(list[2:6])
+# print(list[1:7])
+
+# # list(start:end:step)
 print(list[1:7:2])
 print(list[7:2:-1])
 print(list[7:2:-2])
 print(list[:5])
 print(list[5:])
-print(list[::-1]) # shortcut for reverse traversing a list
-print(list[::2]) # Iterating begin to end with increamentation of index by 2
+# print(list[::-1]) # shortcut for reverse traversing a list
+# print(list[::2]) # Iterating begin to end with increamentation of index by 2
--- JM/Module 03/tuple.py
+++ JM/Module 03/tuple.py
@@ -1,7 +1,19 @@
-def multiple():
-    return 18,1
+# def multiple():
+#     return 18,1
 # print(multiple(),type(multiple()))
+
 things = 'pen','phone','laptop'
+if('laptop' in things):
+    print('laptop is exist')
+things[0] = 'marker'  # tuple are immutable
+print(len(things))
+print(things)
+
+mega = ([1,3,4],[4,10])
+mega[0][1]  = 666 # It changed because the list is mutable, it does'nt matter inside what it is
+print(mega)
+
+
 """
 print(things)
 print(type(things))
@@ -10,10 +22,3 @@ print(len(things[1]))
 print(things[0:3])
 
 """
-if('laptop' in things):
-    print('laptop is exist')
-# things[0] = 'marker'  tuple are immutable
-print(len(things))
-mega = ([1,3,4],[4,10])
-mega[0][1]  = 666 # It changed because the list is mutable, it does'nt matter inside what it is
-print(mega)
