1b336e1cf3eda94fa29e7d9059171f8a61e1ab18
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2024-08-25 09:57:58 +0530

    Update

--- Chapter 5/03_sets.py
+++ Chapter 5/03_sets.py
@@ -1,8 +1,9 @@
 # sets in python is also like the set of c++ but it does not maintain order,
 # Order like increasing or decreasing
-s = {1,5,5,5,32 , "Rakib"};
-e = set() # Don't use s = {} as it will create an empty dictonary
-print(s,type(s));
-s.add(18); # For inserting an element in set
-s.remove(1);
-print(s);
+s = {1, 5, 5, 5, 32, "Rakib"}
+e = set()  # Don't use s = {} as it will create an empty dictonary
+print(s, type(s))
+s.add(18)
+# For inserting an element in set
+s.remove(1)
+print(s)
